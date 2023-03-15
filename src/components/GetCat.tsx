fd56822f480cd929903e94c5c6a69ff59c38e538
Frank Romano <romano.fr@gmail.com>
2023-03-15 10:48:55 -0400

    View adjust on mobile

--- src/components/GetCat.tsx
+++ src/components/GetCat.tsx
@@ -23,17 +23,17 @@ export const GetCat = () => {
                 onClick={cat.getCat}>
                 Show me another cat
             </button>
-            <div className=" w-46 border-sky-500 border-2 p-1 flex flex-row items-center justify-center">
+            <div className=" w-46 border-sky-500 border-2 p-1 flex flex-col items-center justify-center">
                 <div  >
                     <label htmlFor='catSayText' className="text-3md">Say something:</label>
                     <input id='catSayText' type="text" className="m-2 rounded border-sky-500 border-2 hover:border-green-700"
                         value={valueSay} onChange={handleChange} />
                 </div>
-                <div className="" >
+                <div  >
                     <label htmlFor='catTag' className="text-3md">Tag:</label>
                     {/* <input id='catTag' type="text" className="rounded border-sky-500 border-2 hover:border-green-700"
                         value={valueTag} onChange={handleChangeTag} /> */}
-                    <select id='catTag' className="rounded border-sky-500 border-2 hover:border-green-700"
+                    <select id='catTag' className="rounded border-sky-500 m-2 border-2 hover:border-green-700"
                         value={valueTag} onChange={handleChangeTag}>
                         {cat.catTags.map((tag,index)=>(
                             <option key={index}  value={tag}>{tag}</option>
